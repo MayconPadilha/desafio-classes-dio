65928bcc5bdf548926357303f122c8c39b32d184
Maycon Padilha <maycon.jordan5000@gmail.com>
2023-12-21 14:46:05 -0300

    desafio completo

---
+++ README.md
@@ -0,0 +1,30 @@
+#Acesse Online aqui: https://desafio-classificador-dio.vercel.app/
+
+Para esse desafio, fui além, utilizando outras habilidades que já possuía e aprimorando ainda mais meus conhecimentos na área. Em vez de utilizar apenas o console, preferi criar uma interface com um "mini jogo" onde cada batalha ganhamos e perdemos XP, assim adquirindo uma Classificação do herói.
+
+# 3️⃣ Escrevendo as classes de um Jogo
+
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+- Se XP for menor do que 1.000 = Ferro
+- Se XP for entre 1.001 e 2.000 = Bronze
+- Se XP for entre 2.001 e 5.000 = Prata
+- Se XP for entre 6.001 e 7.000 = Ouro
+- Se XP for entre 7.001 e 8.000 = Platina
+- Se XP for entre 8.001 e 9.000 = Ascendente
+- Se XP for entre 9.001 e 10.000= Imortal
+- Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói de nome **{nome}** está no nível de **{nivel}**"
---
+++ jogo.js
@@ -0,0 +1,128 @@
+let heroi = {
+    nome: "",
+    idade: 0,
+    personagem: "",
+    vida: 0,
+    ataque: ''
+  }
+
+let chanceVitoria = 0.7
+let chanceVitoriaBoss = 0.5
+
+function IniciarJogo(){
+    document.getElementById("telaInicio").style.display = "none";
+    document.getElementById("telaJogo").style.display = "block";
+
+    heroi.nome = document.getElementById("name").value; //nome
+    heroi.idade = document.getElementById("idade").value; //idade
+    heroi.personagem = document.getElementById("personagem").value; // personagem
+    heroi.vida = 100
+
+    switch (heroi.personagem) {
+        case "guerreiro":
+            heroi.ataque = 'espada'
+          break;
+        case "mago":
+            heroi.ataque = 'magia'
+          break;
+        case "monge":
+            heroi.ataque = 'artes marciais'
+          break;
+        case "ninja":
+            heroi.ataque = 'shuriken'
+          break;
+        default:
+          alert("Escolha um personagem válido.");
+      }
+    
+    exibirVida();
+
+    let apresentacao = document.getElementById("apresentacao");
+    apresentacao.innerHTML = `<p>Seja Bem-vindo Herói ${heroi[0]}</p>`
+}
+
+function exibirDados(){
+
+    let exibicao = document.getElementById("telaResultado")
+    let exibicaoAtaque = document.getElementById("telaAtaque")
+    exibicaoAtaque.innerHTML = `<p></p>`
+
+    exibicao.innerHTML = `<p>
+    Informacoes do Heroi
+    <br>Nome: ${heroi.nome}
+    <br>Idade: ${heroi.idade}
+    <br>Personagem: ${heroi.personagem}
+    <br>Vida: ${heroi.vida}
+    <br>Tipo de Ataque: ${heroi.ataque}
+    </p>`
+}
+
+function batalha(){
+
+    let exibicao = document.getElementById("telaResultado")
+    let exibicaoAtaque = document.getElementById("telaAtaque")
+
+    exibicaoAtaque.innerHTML = `<p>o ${heroi.personagem} atacou usando ${heroi.ataque}</p>`
+
+    const sorteio = Math.random(); // Gera um número aleatório entre 0 e 1
+    
+    if (sorteio < chanceVitoria) {
+        // Vitória
+        heroi.vida += 35;
+
+        exibirVida();
+
+        exibicao.innerHTML = `<p>Voce venceu a batalha e ganhou ${35} de vida!</p>`
+
+    } else {
+        // Derrota
+        let vidaPerdida = Math.floor(Math.random() * 15) + 10;
+
+        heroi.vida -= vidaPerdida;
+
+        exibirVida();
+
+        exibicao.innerHTML = `<p>Voce perdeu a batalha e perdeu ${vidaPerdida} de vida!</p>`
+        
+    }
+
+    
+}
+
+function desafiarBoss() {
+
+    let exibicao = document.getElementById("telaResultado");
+    let exibicaoAtaque = document.getElementById("telaAtaque")
+
+    exibicaoAtaque.innerHTML = `<p>o ${heroi.personagem} atacou usando ${heroi.ataque}</p>`
+
+    const sorteio = Math.random(); // Gera um número aleatório entre 0 e 1
+
+    if (sorteio < chanceVitoriaBoss) {
+        // Vitória
+        heroi.vida += 300;
+
+        exibirVida();
+
+        exibicao.innerHTML = `<p> Você ganhou desse Boss fraquinho!! Ganhou ${300} de vida!`;
+
+    } else {
+        // Derrota
+        let vidaPerdida = Math.floor(Math.random() * 41) + 60;
+
+        heroi.vida -= vidaPerdida;
+
+        exibirVida();
+
+        exibicao.innerHTML = `<p>Voce perdeu a batalha contra o Boss e perdeu ${vidaPerdida} de vida!</p>`;
+    }
+}
+
+function exibirVida() {
+    let exibirVida = document.getElementById("exibirVida");
+    exibirVida.innerHTML = `<p>Voce tem ${heroi.vida} de vida!</p>`;
+}
+
+function InformacoesPopup() {
+    window.open("info.html", "_blank");
+}
